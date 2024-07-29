ace678f5cbc629cf8e1fa99dc5790ce533e99b64
Elias Tavarez <etav@umich.edu>
2024-07-28 20:33:28 -0500

    feat: add first base asset

--- indagxation/alpha.py
+++ indagxation/alpha.py
@@ -1,24 +1,9 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import pandas as pd
+from dagster import asset
+from langchain_core.documents import Document
+from langchain_community.document_loaders import WebBaseLoader
 
 
-def load_data() -> (
-    tuple[tuple[pd.DataFrame, pd.DataFrame], tuple[pd.DataFrame, pd.DataFrame]]
-):
-    df = datasets.load_iris(as_frame=True)
-    X = df.data
-    y = df.target
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-
-    return (X_train, y_train), (X_test, y_test)
-
-
-def train_model(data: tuple[pd.DataFrame, pd.DataFrame]):
-    lr = LogisticRegression()
-    lr.fit(data[0], data[1])
-    return lr
+@asset
+def load_docs() -> list[Document]:
+    docs = WebBaseLoader("https://google.com").load()
+    return docs
