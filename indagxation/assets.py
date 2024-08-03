69cdd46f4777f20cc8a4573e656f2d61e4cdb870
Elias Tavarez <etav@umich.edu>
2024-08-03 09:23:38 -0500

    feat: add initial asset to retrieve urls with github api

--- indagxation/assets.py
+++ indagxation/assets.py
@@ -1,18 +1,39 @@
+import os
+import requests
+
+
 from dagster import asset, get_dagster_logger
 from langchain_core.documents import Document
 from langchain_community.document_loaders import WebBaseLoader
 
 from .resources import ChromaResource
-from .config import RetrievalConfig, CollectionConfig
+from .config import RetrievalConfig, CollectionConfig, RequestsConfig
 
 logger = get_dagster_logger()
+
+# The goal with this get_docs_urls is to use the github api to get the urls of the different docs
+# We can parse through the different directories and if they are files, we can extract the url
+# We can then pass this url into the next step to load the docs
+
+
+@asset
+def get_doc_urls(config: RequestsConfig):
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    url = f"https://api.github.com/repos/{config.repo}/contents"
+    response = requests.get(url, headers=headers, timeout=config.timeout)
+    logger.debug(response)
+    logger.debug(response.json())
 
 
 @asset
 def load_docs() -> list[Document]:
     """Load documents from the web."""
 
-    docs = WebBaseLoader("https://google.com").load()
+    docs = WebBaseLoader("https://huggingface.co/docs/transformers/index").load()
     logger.debug(f"Loaded {docs} documents from the web.")
     return docs
 
--- indagxation/config.py
+++ indagxation/config.py
@@ -1,6 +1,11 @@
 from dagster import Config
 
 
+class RequestsConfig(Config):
+    repo: str = "dagster-io/dagster"
+    timeout: int = 30
+
+
 class RetrievalConfig(Config):
     query: str
     results: int = 1
